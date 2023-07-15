5a23ab74a4a4b3f44e022c4277f6026ad76729f0
Evan Johnson <evanaterx@gmail.com>
2023-07-14 22:05:14 -0500

    changed capitilization

--- pyclick/pyclick.py
+++ pyclick/pyclick.py
@@ -7,7 +7,7 @@ import pkgutil
 modules = {
     0: 'keyboard',
     1: 'pyautogui',
-    2: 'PyQt5'
+    2: 'pyqt5'
     }
 
 for i in range(len(modules)):
@@ -20,5 +20,19 @@ for i in range(len(modules)):
             print(f"pip3 install {modules[i]} failed")
     else:
         print(f"found {modules[i]} ")
+        
+'''has_keyboard_installed = pkgutil.find_loader("keyboard") is not None
+ 
+os.system("@echo off")
+ 
+if not has_keyboard_installed:
+    try:
+        os.system("pip3 install keyboard")
+        os.system("cls")
+    except ImportError:
+        print("pip3 install keyboard failed.")
+else:
+    print("found")'''
+
 
 input()
