553b4396b26a83ccdf2f63c87f88192f59f05b2a
peenhvh69 <72715920+peenhvh69@users.noreply.github.com>
2023-07-14 22:06:08 -0500

    um fixed evans bad decisions and var naming!!!

--- pyclick.py
+++ pyclick.py
@@ -1,38 +1,26 @@
 
 import ctypes
 import os
-import sys
 import pkgutil
 
-modules = {
+os.system("@echo off")
+print("Pyclick.py by Deepa & peen")
+
+dependencies = {
     0: 'keyboard',
     1: 'pyautogui',
-    2: 'pyqt5'
-    }
+    2: 'PyQt5'
+}
 
-for i in range(len(modules)):
-    installCheck = pkgutil.find_loader(modules[i])
+for i in range(len(dependencies)):
+    installCheck = pkgutil.find_loader(dependencies[i])
 
     if not installCheck:
         try:
-            os.system(f"pip3 install {modules[i]}")
+            os.system(f"pip3 install {dependencies[i]}")
         except ImportError:
-            print(f"pip3 install {modules[i]} failed")
+            print(f"pip3 install {dependencies[i]} failed")
     else:
-        print(f"found {modules[i]} ")
-        
-'''has_keyboard_installed = pkgutil.find_loader("keyboard") is not None
- 
-os.system("@echo off")
- 
-if not has_keyboard_installed:
-    try:
-        os.system("pip3 install keyboard")
-        os.system("cls")
-    except ImportError:
-        print("pip3 install keyboard failed.")
-else:
-    print("found")'''
-
+        print(f"found {dependencies[i]} skipping...")
 
 input()
