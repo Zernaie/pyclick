5f27e50e3ef0c30ebc9747053a95ba68846c912b
peenhvh69 <72715920+peenhvh69@users.noreply.github.com>
2023-07-14 22:44:16 -0500

    window added ty @clyde

--- pyclick.py
+++ pyclick.py
@@ -2,6 +2,7 @@
 import ctypes
 import os
 import pkgutil
+import sys
 
 dependencies = {
     0: 'keyboard',
@@ -19,10 +20,27 @@ for i in range(len(dependencies)):
             print(f"pip3 install {dependencies[i]} failed")
     else:
         print(f"found {dependencies[i]} skipping...")
+        
+from PyQt5.QtWidgets import QApplication, QWidget
 
-from PyQt5.QtWidgets import QApplication, QLabel
-app = QApplication([])
-label = QLabel('Hello World!')
-label.show()
+# Create a subclass of QWidget
+class MyWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setGeometry(100, 100, 300, 200)  # Set the position and size of the window
+        self.setWindowTitle('My Window')  # Set the window title
+        self.show()  # Display the window
+
+# Create an instance of QApplication
+app = QApplication(sys.argv)
+
+# Create an instance of MyWindow
+window = MyWindow()
+
+# Start the event loop
+sys.exit(app.exec_())
 
 input()
